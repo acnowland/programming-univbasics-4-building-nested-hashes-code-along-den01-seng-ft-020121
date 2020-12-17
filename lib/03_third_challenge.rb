b3ca93522b0dea3b58dc4ab0b06f4618b512f664
Adam Nowland <acnowland@gmail.com>
2020-12-17 20:46:17 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -51,7 +51,7 @@ def third_challenge
             :name => "Steven",
             :age => "30", 
             :attitude => "confused"
-          }
+          },
           {
             :name => "Nurse",
             :age => "44",
